7e85a800ca16e60ab2589971e20fc0ac06c75e72
flahartyka <156475178+flahartyka@users.noreply.github.com>
2024-04-29 14:00:12 -0400

    Update vignette.sh

--- vignettes/vignette.sh
+++ vignettes/vignette.sh
@@ -8,16 +8,16 @@ cd llama.cpp
 set -e
 
 MODEL=./llama-2-70b-chat/ggml-model-q4_0.gguf
-MODEL_NAME=management_a
+MODEL_NAME=vignette_answers
 
 # exec options
-opts="--temp 0 -n 300" # additional flags
+opts="--temp 0 -n 300" # additional flags #-n 300
 nl='
 '
-introduction="Provide a management plan for each scenario."
+introduction="Provide a brief medical vignette with the most important clinical information for each scenario."
 
 # file options
-question_file=./managementQA.txt
+question_file=./vignettes_Q.txt
 touch ./llama-2-70b-chat/results/$MODEL_NAME.txt
 output_file=./llama-2-70b-chat/results/$MODEL_NAME.txt
 
@@ -26,13 +26,12 @@ counter=1
 echo 'Running'
 while IFS= read -r question
 do
-  exe_cmd="./main -c 2048- -s 0 -p "\"$introduction$nl$question\"" "$opts" -m ""\"$MODEL\""" >> ""\"$output_file\""
+  exe_cmd="./main -c 2048- -p "\"$introduction$nl$question\"" "$opts" -m ""\"$MODEL\""" >> ""\"$output_file\""
   echo $counter
   echo "Current Question: $question"
   eval "$exe_cmd"
   echo -e "\n------" >> $output_file
   counter=$((counter+1))
 done < "$question_file"
-
 
 
